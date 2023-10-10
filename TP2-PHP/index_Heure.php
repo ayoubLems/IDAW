405adb0e4501b9640f735cb6ec853b4da6482c62
ayoubLems <aayyoouubb-lleemm@oulook.fr>
2023-10-10 09:38:26 +0200

    tp2

---
+++ TP2-PHP/index_Heure.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Heure Courante</title>
+</head>
+<body>
+    <h1>L'heure courante est :</h1>
+    <p><?php echo date('l jS \of F Y h:i:s A'); ?></p>
+</body>
+</html>
